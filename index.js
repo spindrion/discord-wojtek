87b3b468bcfc46c9dbd740e3d7f61e7fa7e2313b
spindrion <szakamoto@gmail.com>
2018-08-31 13:00:16 +0200

    test

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const Discord = require("discord.js");
 const client = new Discord.Client(); 
 const config = require("./config.json");
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+
+express()
+.use(express.static(path.join(__dirname, 'public')))
+.set('views', path.join(__dirname, 'views'))
+.set('view engine', 'ejs')
+.get('/', (req, res) => res.render('pages/index'))
+.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 client.on("ready", () => {
  
